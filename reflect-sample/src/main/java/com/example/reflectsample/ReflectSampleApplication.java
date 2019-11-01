04c6bb3b91cb56156eb9010f8cb8d5a07907b1d1
chendesheng <chendesheng@tuhu.cn>
2019-11-01 16:19:58 +0800

    反射基础学习

---
+++ reflect-sample/src/main/java/com/example/reflectsample/ReflectSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.reflectsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReflectSampleApplication.class, args);
+    }
+
+}
